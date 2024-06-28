668fa266d6dce20e998b92cd44e14fc0cb9883f3
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-06-28 11:26:16 -0600

    Add function to play round of rock paper scissors
    Simulates a round based on human and computer inputs
     - If a winner is found, increments winner's score
     - Returns "human", "computer", or "tie" based on round

--- scripts.js
+++ scripts.js
@@ -38,3 +38,46 @@ function getHumanChoice() {
     console.log(`invalid choice - using "rock" instead`);
     return "rock";
 }
+
+// Plays rock paper scissors
+// Increments scores and returns "human", 
+// "computer", or "tie" based on round
+function playRound(humanChoice, computerChoice) {
+    // If no win condition met, we can assume a tie
+    let winner = "tie"; 
+
+    // Check for win conditions
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "scissors") {
+                humanScore++;
+                winner = "human";
+            } else if (computerChoice === "paper") {
+                computerScore++;
+                winner = "computer";
+            }
+            break;
+            
+        case "paper":
+            if (computerChoice === "rock") {
+                humanScore++;
+                winner = "human";
+            } else if (computerChoice === "scissors") {
+                computerScore++;
+                winner = "computer";
+            }
+            break;
+
+        case "scissors":
+            if (computerChoice === "paper") {
+                winner = "human";
+                humanScore++;
+            } else if (computerChoice === "rock") {
+                computerScore++;
+                winner = "computer";
+            }
+            break;
+    }
+
+    return winner;
+}
