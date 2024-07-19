84d47379f35671780b5b9c528bcd76e55feb3382
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-19 08:12:07 -0600

    Limit number of rounds playable with buttons
    Modify the playRound() function to update total scores for players, and
    stop rounds after a player's total score reaches the required number.
    The result of all rounds is then displayed in console.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 const CHOICES = ["rock", "paper", "scissors"];
+const REQUIRED_WINS = 5;
+
+let humanScore = 0;
+let computerScore = 0;
+let gameOver = false;
 
 const rockButton = document.querySelector('#rock')
 const paperButton = document.querySelector('#paper')
@@ -66,13 +71,28 @@ function getWinner(humanChoice, computerChoice) {
 }
 
 function playRound(humanSelection, computerSelection) {
+    if (gameOver) {
+        return;
+    }
     winner = getWinner(humanSelection, computerSelection);
     if (winner === "human") {
         console.log(`Human Wins! Human: ${humanSelection} | Computer: ${computerSelection}`)
+        humanScore++;
     } else if (winner === "computer") {
         console.log(`Computer Wins! Human: ${humanSelection} | Computer: ${computerSelection}`)
+        computerScore++;
     } else {
         console.log(`Tie! Human: ${humanSelection} | Computer: ${computerSelection}`)
+    }
+
+    if (humanScore >= REQUIRED_WINS) {
+        console.log("Human Wins the Game!");
+        console.log(`Human Total Score: ${humanScore} | Computer Total Score: ${computerScore}`);
+        gameOver = true;
+    } else if (computerScore >= REQUIRED_WINS) {
+        console.log("Computer Wins the Game!");
+        console.log(`Human Total Score: ${humanScore} | Computer Total Score: ${computerScore}`);
+        gameOver = true;
     }
     return winner;
 }
