3abddf6e176292091f8b42ed420c7e93688dc791
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-22 10:14:28 -0600

    Fix missing semicolons

--- scripts.js
+++ scripts.js
@@ -5,18 +5,18 @@ let humanScore = 0;
 let computerScore = 0;
 let gameOver = false;
 
-const rockButton = document.querySelector('#rock')
-const paperButton = document.querySelector('#paper')
-const scissorsButton = document.querySelector('#scissors')
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
 
 const roundResult = document.querySelector('#round-result');
 const humanScoreDisplay = document.querySelector('#human-score');
 const computerScoreDisplay = document.querySelector('#computer-score');
 const gameResult = document.querySelector('#game-result');
 
-rockButton.addEventListener('click', () => playRound("rock", getComputerChoice()))
-paperButton.addEventListener('click', () => playRound("paper", getComputerChoice()))
-scissorsButton.addEventListener('click', () => playRound("scissors", getComputerChoice()))
+rockButton.addEventListener('click', () => playRound("rock", getComputerChoice()));
+paperButton.addEventListener('click', () => playRound("paper", getComputerChoice()));
+scissorsButton.addEventListener('click', () => playRound("scissors", getComputerChoice()));
 
 // Get the computer's choice for a round
 function getComputerChoice() {
@@ -67,15 +67,15 @@ function playRound(humanSelection, computerSelection) {
     winner = getWinner(humanSelection, computerSelection);
 
     if (winner === "human") {
-        roundResult.innerText = (`Human Wins! Human: ${humanSelection} | Computer: ${computerSelection}`)
+        roundResult.innerText = (`Human Wins! Human: ${humanSelection} | Computer: ${computerSelection}`);
         humanScore++;
         humanScoreDisplay.innerText = `Human Score: ${humanScore}`;
     } else if (winner === "computer") {
-        roundResult.innerText = (`Computer Wins! Human: ${humanSelection} | Computer: ${computerSelection}`)
+        roundResult.innerText = (`Computer Wins! Human: ${humanSelection} | Computer: ${computerSelection}`);
         computerScore++;
         computerScoreDisplay.innerText = `Computer Score: ${computerScore}`;
     } else {
-        roundResult.innerText = (`Tie! Human: ${humanSelection} | Computer: ${computerSelection}`)
+        roundResult.innerText = (`Tie! Human: ${humanSelection} | Computer: ${computerSelection}`);
     }
 
     if (humanScore >= REQUIRED_WINS) {
